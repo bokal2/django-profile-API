ea0ad057002dedddc0bf96597f1d47d8eff05434
bokal2 <bobbydasilva98@gmail.com>
2019-08-16 18:44:14 +0300

    Added HomeApiView

--- profiles/profiles_api/views.py
+++ profiles/profiles_api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HomeApiView(APIView):
+    """ Testing API View """
+
+    def get(self, request, format=None):
+        """ Returns a list of APIView features"""
+
+        an_apiview = [
+            'List all the features for rest-framework',
+            'provides flexibility fo business logic'
+        ]
+
+        return Response({
+            "message": "succes",
+            "an_apiview": an_apiview
+        })
+
+
+
+
+
